fa2b734973b46844545da913b6e08d403537e293
jyanga23 <jgyanga20@coe.edu>
2024-04-26 12:57:46 -0500

    created form for to add new squirrel

--- SquirrelSpy_web_frontend/frontend/src/Edit.js
+++ SquirrelSpy_web_frontend/frontend/src/Edit.js
@@ -38,6 +38,41 @@ const Edit = () =>{
         },[]
     )
 
+    /*<div className="Edit-left">
+                    <ul className="list">
+                            <div className="sightings-grid-container">
+                                <li>ID:</li>
+                                <li>User:</li>
+                                <li>Squirrel:</li>
+                                <li>Lat:</li>
+                                <li>Long:</li>
+                                <li>Time:</li>
+                                <li>Certainty_level:</li>
+                                <li>Is_verified:</li>
+                                <li>Verification_comment:</li>
+                                <li>Comment:</li>
+                                <li>Image:</li>
+                            </div>
+                        {sightings.map((sighting, index) => (
+                        <li key={index} className="squirrel-item">
+                            <div className="sightings-grid-container">
+                                <div className="item"><span>{sighting.id}</span></div>
+                                <div className="item"><span>{sighting.user}</span></div>
+                                <div className="item"><span>{sighting.squirrel}</span></div>
+                                <div className="item"><span>{sighting.lat}</span></div>
+                                <div className="item"><span>{sighting.long}</span></div>
+                                <div className="item"><span>{sighting.time}</span></div>
+                                <div className="item"><span>{sighting.certainty_level}</span></div>
+                                <div className="item"><span>{sighting.is_verified}</span></div>
+                                <div className="item"><span>{sighting.verification_comment}</span></div>
+                                <div className="item"><span>{sighting.comment}</span></div>
+                                <div className="item"><span>{sighting.image}</span></div>
+                            </div>
+                        </li>
+                         ))}
+                    </ul>
+                </div>*/
+
 
     return(
         <div>
@@ -63,41 +98,69 @@ const Edit = () =>{
                 </div>
             </div>
 
+            <div className="create-squirrel-container" style={{marginLeft: '30em', marginRight: '25em'}}>
+                <form style={{padding: '3em', alignContent: 'center'}}>
+                <h2>Add New Squirrel</h2>
+                    <label>Name
+                        <br></br>
+                        <input type='text' />
+                    </label>
+                    <br></br>
+                    <div style={{display: 'flex'}}>
+                        <label>Left Ear Color
+                            <br></br>
+                            <select>
+                                <option value="red">Red</option>
+                            </select>
+                        </label>
+                        <label>Right Ear Color
+                            <br></br>
+                            <select>
+                                <option value="blue">Blue</option>
+                            </select>
+                        </label>
+                    </div>
+                    <div>
+                        <label>Tag Serial Number
+                            <br></br>
+                            <input type='text' />
+                        </label>
+                    </div>
+                    <div>
+                        <label>Sex
+                            <br></br>
+                            <select>
+                                <option value="M">Male</option>
+                                <option value="F">Female</option>
+                            </select>
+                        </label>
+                        <label>Age
+                            <br></br>
+                            <input type='number' />
+                        </label>
+                        <label>Weight
+                            <br></br>
+                            <input type='number' />
+                        </label>
+                        <label>Species
+                            <br></br>
+                            <select>
+                                <option value="Brown">Brown</option>
+                                <option value="Black">Black</option>
+                                <option value="Red">Red</option>
+                            </select>
+                        </label>
+                    </div>
+                    <div>
+                        <label>Squirrel Image
+                            <br></br>
+                            <input type='image' />
+                        </label>
+                    </div>
+                </form>
+            </div>
+
              <div className="container">
-                 <div className="Edit-left">
-                    <ul className="list">
-                            <div className="sightings-grid-container">
-                                <li>ID:</li>
-                                <li>User:</li>
-                                <li>Squirrel:</li>
-                                <li>Lat:</li>
-                                <li>Long:</li>
-                                <li>Time:</li>
-                                <li>Certainty_level:</li>
-                                <li>Is_verified:</li>
-                                <li>Verification_comment:</li>
-                                <li>Comment:</li>
-                                <li>Image:</li>
-                                </div>
-                    {sightings.map((sighting, index) => (
-                        <li key={index} className="squirrel-item">
-                            <div className="sightings-grid-container">
-                                <div className="item"><span>{sighting.id}</span></div>
-                                <div className="item"><span>{sighting.user}</span></div>
-                                <div className="item"><span>{sighting.squirrel}</span></div>
-                                <div className="item"><span>{sighting.lat}</span></div>
-                                <div className="item"><span>{sighting.long}</span></div>
-                                <div className="item"><span>{sighting.time}</span></div>
-                                <div className="item"><span>{sighting.certainty_level}</span></div>
-                                <div className="item"><span>{sighting.is_verified}</span></div>
-                                <div className="item"><span>{sighting.verification_comment}</span></div>
-                                <div className="item"><span>{sighting.comment}</span></div>
-                                <div className="item"><span>{sighting.image}</span></div>
-                            </div>
-                        </li>
-                    ))}
-                </ul>
-                </div>
                 <div className="Edit-right">
                     <ul className="list">
                     <div className="squirrels-grid-container">
@@ -124,7 +187,13 @@ const Edit = () =>{
                                 <div className="item"><span>{squirrel.weight}</span></div>
                                 <div className="item"><span>{squirrel.left_ear_color}</span></div>
                                 <div className="item"><span>{squirrel.right_ear_color}</span></div>
-                                <div className="item"><span>{squirrel.image}</span></div>
+                                <span>
+                                    <img
+                                        src={squirrel.image}
+                                        alt={`Squirrel ${squirrel.id}`}
+                                        style={{ width: '150px', height: 'auto' }} // Set width to 100px and auto height to maintain aspect ratio
+                                    />
+                                </span>
                             </div>
                         </li>
                     ))}
